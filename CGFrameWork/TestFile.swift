d82d504967929fc8822c1e0697021156452f97b0
Malli246 <srinivas.ios2017@gmail.com>
2017-09-13 15:49:41 +0530

    cc

---
+++ CGFrameWork/TestFile.swift
@@ -0,0 +1,26 @@
+//
+//  TestFile.swift
+//  CGFrameWork
+//
+//  Created by Vmoksha on 13/09/17.
+//  Copyright © 2017 srinivas. All rights reserved.
+//
+
+import Foundation
+
+
+public class TestFile {
+    
+    
+    public init(){
+        
+        print(" init called ")
+        
+    }
+    
+    public func methodDeclarationInTest(){
+        
+        print(" Test class method called ")
+    }
+    
+}
